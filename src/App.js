bc9b4958d054dc0ef06fa004b3824c399f82cd96
devarsh8519 <devarshdoshi08@gmail.com>
2024-10-28 12:22:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,39 @@
-// import logo from './logo.svg';
+// // import logo from './logo.svg';
+// // import './App.css';
+// // import Navbar from './Pages/Navbar';
+// // import '../node_modules/bootstrap/dist/css/bootstrap.css';
+// // import '../node_modules/bootstrap/dist/js/bootstrap.js';
+// // import './CSS/Custom.css';
+// // import Main from './Pages/Main.js';
+// // import Group from './Pages/Group.js';
+// // import Aboutus from './Pages/Aboutus.js';
+// // import Application from './Pages/Application.js';
+// // import Service from './Pages/Service.js';
+// // import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// // import Products from './Pages/Products.js';
+// // import Home from './Pages/Home.js';
+
+// // function App() {
+// //   return (
+// //     <div className="App">
+// //       <Navbar/>
+// //       {/* <Main/>
+// //       <Group/>
+// //       <Aboutus/>
+// //       <Application/>
+// //       <Service/> */}
+
+// //       <BrowserRouter>
+// //         <Routes>
+// //           <Route path='/' element={<Home/>}></Route>
+// //           <Route path="/products" element={<Products/>}></Route>
+// //         </Routes>
+// //       </BrowserRouter>
+// //     </div>
+// //   );
+// // }
+
+// // export default App;
 // import './App.css';
 // import Navbar from './Pages/Navbar';
 // import '../node_modules/bootstrap/dist/css/bootstrap.css';
@@ -9,50 +44,53 @@
 // import Aboutus from './Pages/Aboutus.js';
 // import Application from './Pages/Application.js';
 // import Service from './Pages/Service.js';
-// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import Products from './Pages/Products.js';
 // import Home from './Pages/Home.js';
+// import Touch from './Pages/Touch.js';
+// import Profile from './Pages/Profile.js';
 
 // function App() {
 //   return (
-//     <div className="App">
-//       <Navbar/>
-//       {/* <Main/>
-//       <Group/>
-//       <Aboutus/>
-//       <Application/>
-//       <Service/> */}
-
-//       <BrowserRouter>
+//     <Router>
+//       <div className="App">
+//         <Navbar />
+        
 //         <Routes>
-//           <Route path='/' element={<Home/>}></Route>
-//           <Route path="/products" element={<Products/>}></Route>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/products" element={<Products />} />
+//           <Route path="/aboutus" element={<Aboutus />} />
+//           <Route path="/application" element={<Application />} />
+//           <Route path="/service" element={<Service />} />
+//           <Route path="/touch" element={<Touch/>}/>
+//           <Route path='/profile' element={<Profile/>}/>
 //         </Routes>
-//       </BrowserRouter>
-//     </div>
+//       </div>
+//     </Router>
 //   );
 // }
 
 // export default App;
+// App.js
 import './App.css';
 import Navbar from './Pages/Navbar';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.js';
 import './CSS/Custom.css';
-import Main from './Pages/Main.js';
-import Group from './Pages/Group.js';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home.js';
+import Products from './Pages/Products.js';
 import Aboutus from './Pages/Aboutus.js';
 import Application from './Pages/Application.js';
 import Service from './Pages/Service.js';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Products from './Pages/Products.js';
-import Home from './Pages/Home.js';
 import Touch from './Pages/Touch.js';
 import Profile from './Pages/Profile.js';
+import ScrollToTop from './Pages/ScrollToTop.js';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="App">
         <Navbar />
         
@@ -60,13 +98,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
-          {/* <Route path="/main" element={<Main />} /> */}
-          {/* <Route path="/group" element={<Group />} /> */}
           <Route path="/aboutus" element={<Aboutus />} />
           <Route path="/application" element={<Application />} />
           <Route path="/service" element={<Service />} />
-          <Route path="/touch" element={<Touch/>}/>
-          <Route path='/profile' element={<Profile/>}/>
+          <Route path="/touch" element={<Touch />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </div>
     </Router>
